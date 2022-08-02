15f089196654c04d8c7c4bb9941925c0e08db8e6
mirkog-navent <mpinas@navent.com>
2022-08-02 09:36:10 -0300

    New component edit text

--- app/src/main/java/com/example/calendarapp/NoteActivity.java
+++ app/src/main/java/com/example/calendarapp/NoteActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ActionBar;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class NoteActivity extends AppCompatActivity {
@@ -22,5 +25,15 @@ public class NoteActivity extends AppCompatActivity {
         selectedDay.setText("Selected day: " + bundle.getString("dayOfMonth"));
         selectedMonth.setText("Selected month: " + bundle.getString("month"));
         selectedYear.setText("Selected year: " + bundle.getString("year"));
+
+        final EditText textInput = findViewById(R.id.textInput);
+        final Button saveTextButton = findViewById(R.id.saveTextButton);
+
+        saveTextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textInput.setText("");
+            }
+        });
     }
 }
